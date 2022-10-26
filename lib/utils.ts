e5f7223a7c5aaf7b6a418aad0e9cfa73d2e39377
Masth0 <dev.mastho@gmail.com>
2022-10-26 22:32:50 +0200

    Translations

--- lib/utils.ts
+++ lib/utils.ts
@@ -127,43 +127,143 @@ export const UITranslationsDefault = {
   title: {
     fr: 'Nous utilisons des cookies',
     en: 'We use cookies',
-    de: 'hre Privatsphäre ist uns wichtig',
+    de: 'Wir verwenden Cookies',
   },
   description: {
     fr: 'Nous utilisons des cookies pour nous assurer du bon fonctionnement de notre site, pour personnaliser notre contenu et nos publicités et afin d’analyser notre trafic. En utilisant ce site Internet ou en fermant ce bandeau, vous consentez à l\'utilisation de ces cookies.',
     en: 'We use cookies to ensure that our site functions properly, to personalize our content and advertising, and to analyze our traffic. By using this website or closing this banner, you consent to the use of these cookies.',
-    de: 'We use cookies to ensure that our site functions properly, to personalize our content and advertising, and to analyze our traffic. By using this website or closing this banner, you consent to the use of these cookies.',
+    de: 'Wir verwenden Cookies, um sicherzustellen, dass unsere Website ordnungsgemäß funktioniert, um unseren Inhalt und unsere Werbung zu personalisieren und um unseren Datenverkehr zu analysieren. Durch die Nutzung dieser Website oder das Schließen dieses Banners stimmen Sie der Verwendung dieser Cookies zu.',
   },
   btn: {
     params: {
       fr: 'Gérer les préférences',
       en: 'Manage my preferences',
-      de: 'Einstellungen ändern',
+      de: 'Präferenzen verwalten',
     },
     closeParams: {
       fr: 'Fermer les préférences',
-      en: 'Close my preferences',
-      de: 'Einstellungen Schließen',
+      en: 'Close preferences',
+      de: 'Präferenzen schliessen',
     },
     reject: {
-      fr: 'Je refuse',
-      en: 'I refuse',
-      de: 'Ich lehne ab',
+      fr: 'Refuser',
+      en: 'Deny',
+      de: 'Ablehnen',
     },
     saveAll: {
-      fr: 'J\'accepte',
-      en: 'I accept',
+      fr: 'Accepter',
+      en: 'Accept',
       de: 'Alle akzeptieren',
     },
     save: {
       fr: 'Enregistrer la sélection',
       en: 'Save selection',
-      de: 'Einstellungen speichern',
+      de: 'Auswahl speichern',
     },
     continue: {
       fr: 'Continuer sans accepter',
       en: 'Continue without accepting',
-      de: 'Fortfahren ohne zu akzeptieren',
+      de: 'Ohne Zustimmung fortfahren',
     },
+  },
+  categories: {
+    mandatory: {
+      title: {
+        fr: 'Obligatoire',
+        en: 'Mandatory',
+        de: 'Erforderlich'
+      },
+      description: {
+        fr: 'Cookies requis pour le bon fonctionnement du site.',
+        en: 'We use required cookies to perform essential website functions.',
+        de: ''
+      }
+    }
   }
 };
+
+
+/*
+
+import CookieConsent from '@masth0/cookie-consent';
+
+// Cookie consent
+const locale = document.documentElement.getAttribute('lang') || 'en';
+const consentTranslations = CookieConsent.UITranslationsDefault;
+consentTranslations.title.de = "Wir verwenden Cookies"
+consentTranslations.description = {
+  fr: "Nous utilisons des cookies pour nous assurer du bon fonctionnement de notre site, pour personnaliser notre contenu et nos publicit\xE9s et afin d\u2019analyser notre trafic. En utilisant ce site Internet ou en fermant ce bandeau, vous consentez \xE0 l'utilisation de ces cookies.",
+  en: "We use cookies to make sure our website works properly, to personalise our content and advertising and to analyse our traffic. By using this website or closing this banner, you consent to the use of these cookies.",
+  de: "Wir verwenden Cookies, um sicherzustellen, dass unsere Website ordnungsgemäß funktioniert, um unseren Inhalt und unsere Werbung zu personalisieren und um unseren Datenverkehr zu analysieren. Durch die Nutzung dieser Website oder das Schließen dieses Banners stimmen Sie der Verwendung dieser Cookies zu."
+};
+consentTranslations.btn.continue.de = 'fortsetzen ohne akzeptieren';
+consentTranslations.btn.reject = {
+  fr: "Je refuse",
+  en: "Deny",
+  de: "ablehnen"
+};
+consentTranslations.btn.saveAll = {
+  fr: "J'accepte",
+  en: "Accept",
+  de: "Alle akzeptieren"
+};
+consentTranslations.btn.continue.de = "Ohne Zustimmung fortfahren";
+consentTranslations.btn.params.de = "Präferenzen verwalten";
+consentTranslations.btn.closeParams.de = "Präferenzen schliessen";
+consentTranslations.btn.save.de = "Auswahl speichern";
+consentTranslations.btn.closeParams.en = "Close preferences";
+
+const translations = {
+  mandatoryCategory: {
+    title: {
+      fr: 'Obligatoire',
+      en: 'Mandatory',
+      de: 'Erforderlich'
+    },
+  },
+  sessionId: {
+    fr: {
+      title: 'Identifiant de session',
+      description: 'Permet de connaitre l\'identifiant de session. La session est utilisée pour la navigation et la sécurité des formulaires (CSRF) PHPSESSID',
+    },
+    en: {
+      title: 'Session ID',
+      description: 'Allows to know the session identifier. The session is used for navigation and form security (CSRF) PHPSESSID',
+    },
+    de: {
+      title: 'Sitzung ID',
+      description: 'Ermöglicht es, die Sitzungs-ID zu erfahren. Die Sitzung wird für die Navigation und die Formularsicherheit (CSRF) verwendet PHPSESSID',
+    },
+  }
+}
+
+const mandatory = CookieConsent.createCategory(translations.mandatoryCategory.title[locale], '');
+mandatory.addCookie({
+  accepted: true,
+  revocable: false,
+  name: translations.sessionId[locale].title,
+  description: translations.sessionId[locale].description,
+  scripts: [],
+  tokens: ['PHPSESSID']
+});
+
+CookieConsent.setup({
+  localeFallback: 'en',
+  version: 1,
+  categories: [
+    mandatory
+  ],
+  forceReload: true,
+  translations: consentTranslations,
+  onSave: () => {},
+  onReject: () => {},
+}).then((consent) => {
+  const $btnOpenConsent = document.querySelector('[data-cc-show]');
+  if ($btnOpenConsent) {
+    $btnOpenConsent.addEventListener('click', function() {
+      consent.show();
+    })
+  }
+});
+
+*/
